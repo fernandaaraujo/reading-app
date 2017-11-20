50d1f0dbc7dc5c8ae3ae1791ddaadd266387c7c6
Fernanda Araujo <nanda.arf@gmail.com>
2017-11-20 16:56:21 -0200

    feat: creates list categories component

---
+++ src/components/ListCategories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const ListCategories = ({ categories }) => {
+  return (
+    <div className="b-container">
+      <ul className="list-categories">
+        { categories.map(category => (
+          <li className="list-categories__item" key={category.name}>
+            <Link to={`/${category.name}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ListCategories.propTypes = {
+  categories: PropTypes.array
+};
+
+export default ListCategories;
