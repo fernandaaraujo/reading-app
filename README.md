5007584cd2966f2ed22cbc1e633467136f12c458
Fernanda Araujo <nanda.arf@gmail.com>
2017-11-20 16:48:30 -0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Setup and running server
+* clone the project on `https://github.com/udacity/reactnd-project-readable-starter` with the command `git clone https://github.com/udacity/reactnd-project-readable-starter.git`
+* go to the folder `cd reactnd-project-readable-starter`
+* install the project dependencies `npm install`
+* run the server `node server`
+
+## Setup and running aplication
+* clone the project on `https://github.com/fernandaaraujo/reading-app.git` with the command `git clone https://github.com/fernandaaraujo/reading-app.git`
+* go to the folder `cd reading-app`
+* install the project dependencies `npm install`
+* run the project `npm start`
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from '../reducers/reducers';
+import App from './App';
+
+import './index.css';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
