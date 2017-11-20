64940731e03a71f7b184c19f21eb0aa70e78f212
Fernanda Araujo <nanda.arf@gmail.com>
2017-11-20 16:50:31 -0200

    feat: creates all routes for the app

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+import { getCategories } from './actions/categories';
+import { getPosts } from './actions/posts';
+
+import HomePage from './components/HomePage';
+import CategoryDetails from './components/CategoryDetails';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+import PostDetails from './components/PostDetails';
+import CreateComment from './components/CreateComment';
+import EditComment from './components/EditComment';
+
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.getCategories();
+    this.props.getPosts();
+  }
+
+  render() {
+    const { categories, posts } = this.props;
+
+    return (
+      <div className='app'>
+        <Switch>
+          <Route exact path='/' render={() => (
+            <HomePage
+              categories={categories}
+              posts={posts}
+            />
+          )} />
+          <Route exact path={'/:category'} component={CategoryDetails} />
+          <Route exact path={'/posts/create'} component={CreatePost} />
+          <Route exact path={'/posts/:postId'} component={PostDetails} />
+          <Route exact path={'/posts/:postId/edit'} component={EditPost} />
+          <Route exact path={'/comments/create'} component={CreateComment} />
+          <Route exact path={'/comments/:commentId/edit'} component={EditComment} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { categories, posts } = state
+  return {
+    categories,
+    posts
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getCategories: () => dispatch(getCategories()),
+    getPosts: () => dispatch(getPosts())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
